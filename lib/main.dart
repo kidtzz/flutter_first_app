4c2dea477b6a9d39de268674bbf4f4be885ae56c
kidtz <bangkitpr4@gmail.com>
2024-01-10 21:16:36 +0700

    first-commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+//flutter run -d chrome
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key, super? key})
+}
